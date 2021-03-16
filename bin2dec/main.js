c1f43ec7505ccb071d003d5ef523da165986c29a
jean dos santos <jeanddg@hotmail.com>
2021-03-15 21:46:39 -0300

    bin2dec

---
+++ bin2dec/main.js
@@ -0,0 +1,38 @@
+const bin = document.querySelector('input[name="bin"]');
+const dec = document.querySelector('input[name="dec"]');
+
+bin.addEventListener('keyup', e => {
+
+    const bin = e.target;
+    
+    const inputValue = bin.value.charAt(bin.value.length - 1);
+
+    if(bin.value.length === 0) return;
+
+    if(bin.value.length > 8) {
+        bin.value = bin.value.slice(0, -1);
+        return;
+    }
+
+    if(isNaN(bin.value)) {
+        alert('É necessário inserir um número (0 ou 1)!');
+        bin.value = bin.value.slice(0, -1);
+        return;
+    } else if(inputValue !== '0' && inputValue !== '1') {
+        alert('É necessário inserir um número (0 ou 1)!');
+        bin.value = bin.value.slice(0, -1);  
+        return;      
+    }
+    
+    let result = 0;
+    let exponent = 0;
+
+    bin.value.split("").reverse().forEach((v, i, array) => {
+        result += (Math.pow(2, exponent)) * parseInt(v);
+        ++exponent;
+    });
+    //dec.value = result.join('');
+
+    dec.value = (!dec.value.length === 0) ? result + parseInt(dec.value) : result;
+
+});
